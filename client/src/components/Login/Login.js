f5516907d78e7af7bf39b394cf0062819416633e
tomasbarcojo <tomasbarcojo@gmail.com>
2021-01-07 21:46:37 -0300

    Added logo on login page and some changes

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,35 +1,15 @@
-// import React from 'react'
-// import TextField from '@material-ui/core/TextField';
-// import '../../App.css'
-
-// export default function Login() {
-//     return (
-//         <div>
-//             <form>
-//                 <div className='LoginContainer'>
-//                     <TextField id="outlined-basic" label="Email" variant="outlined" />
-//                     <TextField id="outlined-basic" label="Contraseña" variant="outlined" />
-//                 </div>
-//             </form>
-//         </div>
-//     )
-// }
-
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
+import { Link } from 'react-router-dom'
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Copyright from '../../utils/Copyrigth'
+import WisproLogo from '../../images/wisprologohoriz.png'
 import '../../App.css'
 
 const useStyles = makeStyles((theme) => ({
@@ -38,7 +18,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: '100%',  
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -50,65 +30,68 @@ export default function SignIn() {
   const classes = useStyles();
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className='loginPaper'>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Inicio de sesion
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Contraseña"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                ¿Olvidó su contraseña?
-              </Link>
+    <div className='loginPage'>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className='loginPaper'>
+          {/* <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar> */}
+          <img className='WisproLogo' src={WisproLogo} alt='Wispro Logo'/>
+          <Typography component="h1" variant="h5">
+            Inicio de sesion
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Contraseña"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+
+            <Grid container>
+              <Grid item xs>
+                <Link to="/recover">
+                  ¿Olvidó su contraseña?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link to="/register">
+                  {"¿No tiene una cuenta? Regístrese"}
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"¿No tiene una cuenta? Regístrese"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
+    </div>
   );
 }
