74124e9f05c97833f793ab9ffddc99716fbb8f4b
tomasbarcojo <tomasbarcojo@gmail.com>
2021-01-08 20:20:58 -0300

    Remove warnings

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -7,7 +7,6 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import Copyright from '../../utils/Copyrigth'
 import WisproLogo from '../../images/wisprologohoriz.png'
 
--- client/src/utils/Copyrigth.js
+++ client/src/utils/Copyrigth.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Typography from '@material-ui/core/Typography';
 
 export default function Copyrigth() {
     return (
