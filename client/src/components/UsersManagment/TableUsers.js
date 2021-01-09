6a97ba7782c8c94c744cb9f7d1db4fe4e8f6a470
tomasbarcojo <tomasbarcojo@gmail.com>
2021-01-09 15:57:03 -0300

    Emails not showing on table

--- client/src/components/UsersManagment/TableUsers.js
+++ client/src/components/UsersManagment/TableUsers.js
@@ -31,27 +31,26 @@ const users = [
     },
 ]
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
+function createData(name, email) {
+  return { name, email };
 }
 
 const rows = [
-  createData('India', 'email@email.com', 1324171354, 3287263),
-  createData('China', 'email@email.com', 1403500365, 9596961),
-  createData('Italy', 'email@email.com', 60483973, 301340),
-  createData('United States', 'email@email.com', 327167434, 9833520),
-  createData('Canada', 'email@email.com', 37602103, 9984670),
-  createData('Australia', 'email@email.com', 25475400, 7692024),
-  createData('Germany', 'email@email.com', 83019200, 357578),
-  createData('Ireland', 'email@email.com', 4857000, 70273),
-  createData('Mexico', 'email@email.com', 126577691, 1972550),
-  createData('Japan', 'email@email.com', 126317000, 377973),
-  createData('France', 'email@email.com', 67022000, 640679),
-  createData('United Kingdom', 'email@email.com', 67545757, 242495),
-  createData('Russia', 'email@email.com', 146793744, 17098246),
-  createData('Nigeria', 'email@email.com', 200962417, 923768),
-  createData('Brazil', 'email@email.com', 210147125, 8515767),
+  createData('India', 'email@email.com'),
+  createData('China', 'email@email.com'),
+  createData('Italy', 'email@email.com'),
+  createData('United States', 'email@email.com'),
+  createData('Canada', 'email@email.com'),
+  createData('Australia', 'email@email.com'),
+  createData('Germany', 'email@email.com'),
+  createData('Ireland', 'email@email.com'),
+  createData('Mexico', 'email@email.com'),
+  createData('Japan', 'email@email.com'),
+  createData('France', 'email@email.com'),
+  createData('United Kingdom', 'email@email.com'),
+  createData('Russia', 'email@email.com'),
+  createData('Nigeria', 'email@email.com'),
+  createData('Brazil', 'email@email.com'),
 ];
 
 const useStyles = makeStyles({
@@ -77,6 +76,8 @@ export default function TableUsers() {
     setPage(0);
   };
 
+  console.log(rows)
+
   return (
     <Paper className={classes.root}>
       <TableContainer className={classes.container}>
@@ -98,18 +99,14 @@ export default function TableUsers() {
             {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
               return (
                 <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {users.map((column) => {
+                  {columns.map((column) => {
                     const value = row[column.id];
                     return (
-                      <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                      <TableCell key={column.id}>
+                        {/* {column.format && typeof value === 'number' ? column.format(value) : value} */}
+                        {value}
                       </TableCell>
                     );
-                    // return (
-                    //     <TableCell key={column.id} aling={column.aling}>
-                    //         <EditIcon />
-                    //     </TableCell>
-                    // )
                   })}
                 </TableRow>
               );
